15a7082dce18c98a2b2b3f53ae8d1c778e35fd46
Julien Bischoff <julien.bischoff@viacesi.fr>
2018-10-30 16:14:41 +0100

    Piege Borne
    Ajout du piège de la borne

---
+++ Assets/Standard Assets/2D/Scripts/Decollage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decollage : MonoBehaviour
+{
+
+    private Rigidbody2D player_rigidbody2D;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            player_rigidbody2D = other.GetComponent<Rigidbody2D>();
+            player_rigidbody2D.AddForce(new Vector2(0f, 2000f));
+
+        }
+    }
+}
