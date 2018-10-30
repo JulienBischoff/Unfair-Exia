b4979f5d176a9946691a419206d45c189c7f64a7
Julien Bischoff <julien.bischoff@viacesi.fr>
2018-10-30 15:41:03 +0100

    Base du projet
    Transfert vers Github

---
+++ Assets/Standard Assets/2D/Scripts/Trap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+
+    public class Trap : MonoBehaviour
+    {
+        public Transform piege;
+        public Vector2 coordonnees;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                Instantiate(piege,(coordonnees), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Standard Assets/2D/Scripts/TrapPlateform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPlateform : MonoBehaviour {
+
+    public GameObject plateform;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player")
+        {
+            plateform.GetComponent<BoxCollider2D>().isTrigger = true;
+            plateform.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+
+    }
+	
+}
