e3e4fe1fd4f8c7861ebbcc55e645c47bfddd1e06
Julien Bischoff <julien.bischoff@viacesi.fr>
2018-11-07 13:46:15 +0100

    Death Screen
    Ajout d'un Death Screen + canvas

---
+++ Assets/Standard Assets/2D/Scripts/DeathScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScreen : MonoBehaviour
+{
+
+    public GameObject canvas;
+
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+    }
+
+}
---
+++ Assets/Standard Assets/2D/Scripts/RestartInDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartInDeath : MonoBehaviour {
+
+	// Use this for initialization
+	public void RESTART()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+    }
+}
