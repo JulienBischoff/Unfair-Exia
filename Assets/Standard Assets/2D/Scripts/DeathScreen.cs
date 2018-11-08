c5634b44e53edce994f355bca5743cd9f3b31faf
Julien Bischoff <julien.bischoff@viacesi.fr>
2018-11-08 13:26:34 +0100

    Reglage problème ecran de mort
    Ajout d'un Game Manager pour gerer l'ecran de mort

--- Assets/Standard Assets/2D/Scripts/DeathScreen.cs
+++ Assets/Standard Assets/2D/Scripts/DeathScreen.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class DeathScreen : MonoBehaviour
 {
 
-    public GameObject canvas;
-
     void Start()
     {
-
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            canvas.SetActive(true);
-            Time.timeScale = 0;
+            FindObjectOfType<GameManager>().Death();
         }
 
     }
---
+++ Assets/Standard Assets/2D/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject canvas;
+
+    public void Death()
+    {
+        canvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+
+}
