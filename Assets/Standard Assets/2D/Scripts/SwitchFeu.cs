4f818b9c1d911160ad63017caede33ef11b1b301
Julien Bischoff <julien.bischoff@viacesi.fr>
2018-11-09 15:35:50 +0100

    Ajout feu et Test
    Test du switch du feu

---
+++ Assets/Standard Assets/2D/Scripts/SwitchFeu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchFeu : MonoBehaviour {
+
+    public GameObject feu;
+    public Sprite feuRouge;
+    public Sprite feuVert;
+    bool isVert;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            if (isVert)
+            {
+                feu.GetComponent<SpriteRenderer>().sprite = feuRouge;
+                isVert = !isVert;
+            }
+            else
+            {
+                feu.GetComponent<SpriteRenderer>().sprite = feuVert;
+                isVert = !isVert;
+            }
+        }
+    }
+}
