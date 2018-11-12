2d227ec99e41c3002b0b1987681c99dca20b96ac
Julien Bischoff <julien.bischoff@viacesi.fr>
2018-11-12 14:55:23 +0100

    Rajout Feu fonctionnel
    Possible de mourir dans la zone entre les feux

--- Assets/Standard Assets/2D/Scripts/GameManager.cs
+++ Assets/Standard Assets/2D/Scripts/GameManager.cs
@@ -6,12 +6,37 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     public GameObject canvas;
+    public bool isVert;
+
+    private int time;
+
+
+    public void Start()
+    {
+        time = 0;
+    }
+
+
+    public void Update()
+    {
+        time++;
+        if(time>0 && time<500){
+            isVert = true;
+        }
+        else if(time>=500 && time<1000)
+        {
+            isVert = false;
+        }
+        else
+        {
+            time = 0;
+        }
+    }
 
     public void Death()
     {
         canvas.SetActive(true);
         Time.timeScale = 0;
     }
-
 
 }
--- Assets/Standard Assets/2D/Scripts/SwitchFeu.cs
+++ Assets/Standard Assets/2D/Scripts/SwitchFeu.cs
@@ -7,34 +7,23 @@ public class SwitchFeu : MonoBehaviour {
     public GameObject feu;
     public Sprite feuRouge;
     public Sprite feuVert;
-    bool isVert;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    bool vert;
 
 
-    private void OnTriggerEnter2D(Collider2D other)
+
+    // Update is called once per frame
+    void Update()
     {
-
-        if (other.tag == "Player")
+        vert = FindObjectOfType<GameManager>().isVert;
+        if (vert)
         {
-            if (isVert)
-            {
-                feu.GetComponent<SpriteRenderer>().sprite = feuRouge;
-                isVert = !isVert;
-            }
-            else
-            {
-                feu.GetComponent<SpriteRenderer>().sprite = feuVert;
-                isVert = !isVert;
-            }
+            feu.GetComponent<SpriteRenderer>().sprite = feuVert;
         }
+        else if (!vert)
+        {
+            feu.GetComponent<SpriteRenderer>().sprite = feuRouge;
+        }
+        
     }
 }
